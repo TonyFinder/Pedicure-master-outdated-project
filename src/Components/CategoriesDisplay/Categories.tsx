8b8ec4e7ee736830cafd3b7db9a01778821cfaa0
Anton <antonrozdobudko@gmail.com>
2021-11-27 18:13:42 +1000

    App is finished for publishing.

--- src/Components/CategoriesDisplay/Categories.tsx
+++ src/Components/CategoriesDisplay/Categories.tsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 import {CategoriesType, DescriptionItemsType, ItemsType} from '../../Store/Store';
 import {CategoryList} from './CategoryList/CategoryList';
-import s from './Categories.module.css'
+import {AppBar, Button, ButtonGroup, Container, Grid, Toolbar, Typography} from '@material-ui/core';
+import {ArrowRightAlt, FavoriteTwoTone} from '@material-ui/icons';
 
 type CategoriesPropsType = {
     store: DescriptionItemsType & ItemsType & CategoriesType
@@ -9,7 +10,7 @@ type CategoriesPropsType = {
 
 export const Categories = (props: CategoriesPropsType) => {
     const [menu, setMenu] = useState<boolean>(true)
-    const [category, setCategory] = useState<string>("Сухая кожа стопы")
+    const [category, setCategory] = useState<string>('Сухая кожа стопы')
     const onClickHandler = (category: string) => {
         setCategory(category)
         setMenu(false)
@@ -17,17 +18,60 @@ export const Categories = (props: CategoriesPropsType) => {
 
     return (
         <div>
-            { menu ?
-                (<div>{props.store.categories.map(category =>
-                    <div key={category.id}>
-                        <input value={category.category} type={'button'}
-                               onClick={() => onClickHandler(category.category)}/>
-                    </div>)}
-                </div>)
+            <AppBar position={'sticky'}>
+                <Toolbar>
+                    <Grid container spacing={1} justifyContent={'center'} alignItems={'center'}
+                          style={{margin: '3px 0px'}}>
+                        <Grid item>
+                            <Grid container>
+                                <Typography variant="h6" style={{fontFamily: 'cursive'}}>Жми
+                                    сюда</Typography>
+                                <ArrowRightAlt fontSize={'large'} style={{margin: 'auto'}}/>
+                            </Grid>
+                        </Grid>
+                        <Grid item>
+                            <Button color="inherit" variant={'outlined'}
+                                    onClick={() => setMenu(true)}>Категории</Button>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+            {menu ?
+                (
+                    <Container maxWidth={'md'}>
+                        <Grid container>
+                            <Grid container justifyContent={'center'} style={{margin: '10px 0px'}}>
+                                <Typography align={'center'} variant="h6"
+                                            style={{fontFamily: 'cursive', fontWeight: 'bolder'}}>Выберите
+                                    категорию:</Typography>
+                            </Grid>
+                            <Grid container justifyContent={'center'}>
+                                <ButtonGroup color={'primary'} orientation={'vertical'}
+                                             aria-label={'vertical outlined button group'} variant={'text'}>
+                                    {props.store.categories.map(category =>
+                                        <Button key={category.id}
+                                                onClick={() => onClickHandler(category.category)}>{category.category}</Button>
+                                    )}
+                                </ButtonGroup>
+                            </Grid>
+                            <Grid container justifyContent={'center'}>
+                                <img alt={'backgroundImage'} style={{opacity: '0.3'}}
+                                     src={'https://live.staticflickr.com/65535/51706322654_f53b3c9628_n.jpg'}/>
+                            </Grid>
+                            <Grid container justifyContent={'center'} alignItems={'center'}
+                                  style={{margin: '10px 0px'}}>
+                                <Typography align={'center'} variant="h6"
+                                            style={{marginRight: '5px', fontFamily: 'cursive'}}>Ваш
+                                    Pedicure.cabinet</Typography>
+                                <FavoriteTwoTone/>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                )
                 :
-                <div className={s.blocks}>
-                <CategoryList store={props.store} category={category} callbackForMenu={() => setMenu(!menu)}/>
-                </div>
+                <Container maxWidth={'md'}>
+                    <CategoryList store={props.store} category={category}/>
+                </Container>
             }
         </div>
     )
--- src/Components/CategoriesDisplay/CategoryList/CategoryList.tsx
+++ src/Components/CategoriesDisplay/CategoryList/CategoryList.tsx
@@ -1,49 +1,89 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {v1} from 'uuid';
 import {StoreType} from '../../../Store/Store';
+import {Button, Grid, Paper} from '@material-ui/core';
 
 export type CategoryListPropsType = {
     store: StoreType
     category: string
-    callbackForMenu: () => void
 }
 
 export const CategoryList = (props: CategoryListPropsType) => {
     const specialProductsItems = props.store.items.filter(f => f.category === props.category)
-    const onClickBackToMenu = () => {
-        props.callbackForMenu()
+    const [ingredients, setIngredients] = useState<boolean>(false)
+    const [apply, setApply] = useState<boolean>(false)
+
+    const onClickIngredients = () => {
+        if (ingredients) {
+            setIngredients(false)
+            setApply(false)
+        } else {
+            setIngredients(true)
+            setApply(false)
+        }
+    }
+    const onClickApply = () => {
+        if (apply) {
+            setApply(false)
+            setIngredients(false)
+        } else {
+            setApply(true)
+            setIngredients(false)
+        }
     }
 
     return (
-        <div>
-            <button onClick={onClickBackToMenu}>Назад к категориям</button>
+        <Grid container justifyContent={'center'} spacing={1}>
             {specialProductsItems.map(m => {
                 return (
-                    <div>
-                        <h3>
-                            {`${m.name} Объём ${m.volume} мл.`}
-                        </h3>
-                        <img src={props.store.description.find(f => f.id === m.id)?.picture} alt={"Изображение"}/>
-                        <div>
-                            <h4>Действие:</h4>
-                            <ul>
-                                {props.store.description.find(f => f.id === m.id)?.action.map(m1 => <li>{m1}</li>)}
-                            </ul>
-                            <h4>Ингридиенты:</h4>
-                            <ul>
-                                {props.store.description.find(f => f.id === m.id)?.ingredients.map(m2 => <li>{m2}</li>)}
-                            </ul>
-                            <h4>Способ применения:</h4>
-                            <ul>
-                                {props.store.description.find(f => f.id === m.id)?.apply}
-                            </ul>
-                            <h4>Цены:</h4>
-                            <div><span>Регулярная - <b>{m.priceRegular} рублей</b></span></div>
-                            <div><span> В день процедуры - <b>{m.priceDiscounted} рублей</b> (скидка от регулярной цены - <b>{m.discount})</b></span></div>
-                        </div>
-                    </div>
+                    <Grid item key={m.id}>
+                        <Paper elevation={6} style={{padding: '15px'}}>
+                            <Grid container alignItems={'center'} justifyContent={'center'} direction={'column'}>
+                                <Grid item style={{textAlign: 'center'}}>
+                                    <h3 style={{margin: '0px'}}>
+                                        {m.name}
+                                    </h3>
+                                    <span style={{fontStyle: 'italic', fontSize: '15px'}}>Объём {m.volume} мл.</span>
+                                </Grid>
+                                <Grid item style={{margin: '10px 0px'}}>
+                                    <img src={props.store.description.find(f => f.id === m.id)?.picture}
+                                         alt={'Изображение'}/>
+                                </Grid>
+                                <Grid item>
+                                    <div>Цена - <b>{m.priceRegular} руб.</b></div>
+                                    <div>В день процедуры - <b>{m.priceDiscounted} руб.</b></div>
+                                </Grid>
+                            </Grid>
+                            <Grid container style={{paddingTop: '25px'}}>
+                                <Grid item>
+                                    <h4 style={{margin: '0px'}}>Действие:</h4>
+                                    <ul>
+                                        {props.store.description.find(f => f.id === m.id)?.action.map(m1 => <li
+                                            key={v1()}>{m1}</li>)}
+                                    </ul>
+                                </Grid>
+                                <Grid container justifyContent={'center'}>
+                                    <Button variant={'outlined'} color={'primary'}
+                                            style={{margin: '2px'}}
+                                            onClick={onClickIngredients}>Ингридиенты</Button>
+                                    <Button variant={'outlined'} color={'primary'}
+                                            style={{margin: '2px'}}
+                                            onClick={onClickApply}>Способ применения</Button>
+                                </Grid>
+                                {ingredients && <Grid item>
+                                    <ul>
+                                        {props.store.description.find(f => f.id === m.id)?.ingredients.map(m2 => <li
+                                            key={v1()}>{m2}</li>)}
+                                    </ul>
+                                </Grid>}
+                                {apply && <Grid item>
+                                    <div style={{textAlign: 'center', paddingTop: '15px'}}>{props.store.description.find(f => f.id === m.id)?.apply}</div>
+                                </Grid>}
+                            </Grid>
+                        </Paper>
+                    </Grid>
                 )
             })}
-            <button onClick={onClickBackToMenu}>Назад к категориям</button>
-        </div>
+        </Grid>
     )
 }
