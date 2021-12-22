3b3b9bb0dcc8bce06dac5f0083c3f6f1352979a8
Anton <antonrozdobudko@gmail.com>
2021-12-22 13:20:50 +1000

    Small changes.

--- src/Components/CategoriesDisplay/CategoryList/CategoryList.tsx
+++ src/Components/CategoriesDisplay/CategoryList/CategoryList.tsx
@@ -16,23 +16,47 @@ export type ValueType = {
 
 export const CategoryList = (props: CategoryListPropsType) => {
     const specialProductsItems = props.store.items.filter(f => f.category === props.category)
-    const [buttonsValue, setButtonsValue] = useState<ValueType[]>([{ id: 372, valueAction: true, valueIngredients: false, valueApply: false},{ id: 306, valueAction: true, valueIngredients: false, valueApply: false},{ id: 832, valueAction: true, valueIngredients: false, valueApply: false},{ id: 1111, valueAction: true, valueIngredients: false, valueApply: false},{ id: 991312, valueAction: true, valueIngredients: false, valueApply: false},{ id: 889, valueAction: true, valueIngredients: false, valueApply: false},{ id: 367, valueAction: true, valueIngredients: false, valueApply: false},{ id: 888, valueAction: true, valueIngredients: false, valueApply: false},{ id: 369, valueAction: true, valueIngredients: false, valueApply: false},{ id: 991039, valueAction: true, valueIngredients: false, valueApply: false},{ id: 111, valueAction: true, valueIngredients: false, valueApply: false},{ id: 222, valueAction: true, valueIngredients: false, valueApply: false},{ id: 223, valueAction: true, valueIngredients: false, valueApply: false},])
+    const [buttonsValue, setButtonsValue] = useState<ValueType[]>([
+            {id: 372, valueAction: true, valueIngredients: false, valueApply: false}, {
+            id: 306,
+            valueAction: true,
+            valueIngredients: false,
+            valueApply: false
+        }, {id: 832, valueAction: true, valueIngredients: false, valueApply: false}, {
+            id: 1111,
+            valueAction: true,
+            valueIngredients: false,
+            valueApply: false
+        }, {id: 991312, valueAction: true, valueIngredients: false, valueApply: false}, {
+            id: 889,
+            valueAction: true,
+            valueIngredients: false,
+            valueApply: false
+        }, {id: 367, valueAction: true, valueIngredients: false, valueApply: false}, {
+            id: 888,
+            valueAction: true,
+            valueIngredients: false,
+            valueApply: false
+        }, {id: 369, valueAction: true, valueIngredients: false, valueApply: false}, {
+            id: 991039,
+            valueAction: true,
+            valueIngredients: false,
+            valueApply: false
+        }, {id: 111, valueAction: true, valueIngredients: false, valueApply: false}, {
+            id: 222,
+            valueAction: true,
+            valueIngredients: false,
+            valueApply: false
+        }, {id: 223, valueAction: true, valueIngredients: false, valueApply: false},
+        ])
 
     const onClickChanger = (id: number, valueOfButton: 'valueAction' | 'valueIngredients' | 'valueApply') => {
-        switch (valueOfButton) {
-            case 'valueAction':
-                setButtonsValue(buttonsValue.map(m => m.id === id ? {...m, valueAction: !m.valueAction, valueIngredients: false, valueApply: false} : m))
-                break
-            case 'valueIngredients':
-                setButtonsValue(buttonsValue.map(m => m.id === id ? {...m, valueAction: false, valueIngredients: !m.valueIngredients, valueApply: false} : m))
-                break
-            case 'valueApply':
-                setButtonsValue(buttonsValue.map(m => m.id === id ? {...m, valueAction: false, valueIngredients: false, valueApply: !m.valueApply} : m))
-                break
-            default:
-                console.log('Ошибка')
-                break
-        }
+        setButtonsValue(buttonsValue.map(m => m.id === id ? {
+            ...m,
+            valueAction: valueOfButton === 'valueAction' ? !m.valueAction : false,
+            valueIngredients: valueOfButton === 'valueIngredients' ? !m.valueIngredients : false,
+            valueApply: valueOfButton === 'valueApply' ? !m.valueApply : false
+        } : m))
     }
 
     return (
@@ -40,7 +64,7 @@ export const CategoryList = (props: CategoryListPropsType) => {
             {specialProductsItems.map(m => {
                 return (
                     <Grid item key={m.id}>
-                        <Paper elevation={6} style={{padding: '15px', width: "85vw"}}>
+                        <Paper elevation={6} style={{padding: '15px', width: '85vw'}}>
                             <Grid container alignItems={'center'} justifyContent={'center'} direction={'column'}>
                                 <Grid item style={{textAlign: 'center'}}>
                                     <h3 style={{margin: '0px'}}>
@@ -50,7 +74,7 @@ export const CategoryList = (props: CategoryListPropsType) => {
                                 </Grid>
                                 <Grid item style={{margin: '10px 0px'}}>
                                     <img src={props.store.description.find(f => f.id === m.id)?.picture}
-                                         alt={'Изображение'} style={{height: "300px"}}/>
+                                         alt={'Изображение'} style={{height: '250px'}}/>
                                 </Grid>
                                 <Grid item>
                                     <div>Цена - <b>{m.priceRegular} руб.</b></div>
@@ -60,13 +84,15 @@ export const CategoryList = (props: CategoryListPropsType) => {
                             <Grid container style={{paddingTop: '25px'}}>
                                 <Grid container justifyContent={'center'}>
                                     <ButtonGroup size="small" color={'primary'}>
-                                        <Button variant={buttonsValue.filter(f => f.id === m.id)[0].valueAction ? 'contained' : 'outlined'}
-                                                onClick={() => onClickChanger(m.id, 'valueAction')}>Действие</Button>
-                                        <Button variant={buttonsValue.filter(f => f.id === m.id)[0].valueIngredients ? 'contained' : 'outlined'}
-                                                onClick={() => onClickChanger(m.id, 'valueIngredients')}>Ингредиенты</Button>
-                                        <Button variant={buttonsValue.filter(f => f.id === m.id)[0].valueApply ? 'contained' : 'outlined'}
-                                                onClick={() => onClickChanger(m.id, 'valueApply')}>Как
-                                            применять</Button>
+                                        <Button
+                                            variant={buttonsValue.filter(f => f.id === m.id)[0].valueAction ? 'contained' : 'outlined'}
+                                            onClick={() => onClickChanger(m.id, 'valueAction')}>Действие</Button>
+                                        <Button
+                                            variant={buttonsValue.filter(f => f.id === m.id)[0].valueIngredients ? 'contained' : 'outlined'}
+                                            onClick={() => onClickChanger(m.id, 'valueIngredients')}>Ингредиенты</Button>
+                                        <Button
+                                            variant={buttonsValue.filter(f => f.id === m.id)[0].valueApply ? 'contained' : 'outlined'}
+                                            onClick={() => onClickChanger(m.id, 'valueApply')}>Применение</Button>
                                     </ButtonGroup>
                                 </Grid>
                                 {buttonsValue.filter(f => f.id === m.id)[0].valueAction && <Grid item>
