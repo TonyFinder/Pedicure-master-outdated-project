b82e1591025459a4046eb4b1d3f04ed456c3733c
Anton <antonrozdobudko@gmail.com>
2021-12-30 13:21:37 +1000

    Added instagram image with the link.

--- src/Components/WelcomePage/WelcomePage.tsx
+++ src/Components/WelcomePage/WelcomePage.tsx
@@ -1,6 +1,8 @@
 import {AppBar, Button, Grid, Toolbar, Typography} from '@material-ui/core';
 import React from 'react';
 import {FavoriteTwoTone} from '@material-ui/icons';
+import whatsapp from '../../pictures/whatsapp.jpg'
+import instagram from '../../pictures/instagram.png'
 
 export type WelcomePageType = {
     callbackButton: () => void
@@ -22,11 +24,21 @@ export const WelcomePage = (props: WelcomePageType) => {
                 </Toolbar>
             </AppBar>
             <Grid container justifyContent={'center'} alignItems={'center'} direction={'column'}>
-                <Button onClick={props.callbackButton} variant={'outlined'} color={'primary'} size={'large'} style={{margin: '30px 0px'}}>Средства для ухода в наличии</Button>
-                <Grid item>
-                    <a href={'https://wa.me/79510215025'}><img
-                        src={'https://live.staticflickr.com/65535/51708193253_5cd29eecb5_m.jpg'}
-                        alt={'WhatsApp'} style={{width: '150px'}}/></a>
+                <Button onClick={props.callbackButton} variant={'outlined'} color={'primary'} size={'large'}
+                        style={{margin: '30px 0px'}}>Средства для ухода в наличии</Button>
+                {/*<Grid item>
+                    <Grid container>*/}
+                        <Grid item>
+                            <a href={'https://instagram.com/pedicure.cabinet?utm_medium=copy_link'}><img
+                                src={instagram}
+                                alt={'Instagram'} style={{width: '150px'}}/></a>
+                        </Grid>
+                        <Grid item>
+                            <a href={'https://wa.me/79510215025'}><img
+                                src={whatsapp}
+                                alt={'WhatsApp'} style={{width: '150px'}}/></a>
+                        {/*</Grid>
+                    </Grid>*/}
                 </Grid>
             </Grid>
         </div>
